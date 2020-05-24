a7921fbd3b6baf23a7a9a07642d6b41176eb56e2
FedericoLandaeta <65091166+FedericoLandaeta@users.noreply.github.com>
2020-05-24 11:55:09 -0400

    Removed unwanted special characters

--- Main.java
+++ Main.java
@@ -4,14 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Main{
+public class textAnalyzer{
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("FileAddress");
+        File file = new File("C:\\Users\\User\\Documents\\EclipseFiles\\TheRaven.txt");
         Scanner scanner = new Scanner(file);
         Map<String,Integer> map = new HashMap<String, Integer>(); 
         while (scanner.hasNext())
         {
+        	
             String word = scanner.next(); 
+            word = word.replaceAll("[^a-zA-Z0-9]" , "");
             if(map.containsKey(word) == false) 
                 map.put(word,1);
             else 
@@ -25,7 +27,7 @@ public class Main{
         List<Map.Entry<String, Integer>> sortedList = new ArrayList<Map.Entry<String, Integer>>(set);  
         Collections.sort( sortedList, new Comparator<Map.Entry<String, Integer>>() 
         {
-            public int compare( Map.Entry<String, Integer> a, Map.Entry<String, Integer> b ) 
+            public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b ) 
             {
                 return (b.getValue()).compareTo( a.getValue() );            
             }
