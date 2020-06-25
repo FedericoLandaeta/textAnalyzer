f6a712e36a6af52d1f6cf56d640557f2b43db512
FedericoLandaeta <65091166+FedericoLandaeta@users.noreply.github.com>
2020-06-24 20:37:58 -0400

    Changed to URL scanner class called "Reader"

--- textAnalyzer.java
+++ textAnalyzer.java
@@ -1,39 +1,71 @@
-package textAnalyzer;
+package ravenReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
-public class textAnalyzer{
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\Users\\User\\Documents\\EclipseFiles\\TheRaven.txt");
-        Scanner scanner = new Scanner(file);
-        Map<String,Integer> map = new HashMap<String, Integer>(); 
-        while (scanner.hasNext())
-        {
-        	
-            String word = scanner.next(); 
-            word = word.replaceAll("[^a-zA-Z0-9]" , "");
-            if(map.containsKey(word) == false) 
-                map.put(word,1);
-            else 
-            {
-                int count = (int)(map.get(word)); 
-                map.remove(word); 
-                map.put(word,count+1);
-            }
-        }
-        Set<Map.Entry<String, Integer>> set = map.entrySet();
-        List<Map.Entry<String, Integer>> sortedList = new ArrayList<Map.Entry<String, Integer>>(set);  
-        Collections.sort( sortedList, new Comparator<Map.Entry<String, Integer>>() 
-        {
-            public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b ) 
-            {
-                return (b.getValue()).compareTo( a.getValue() );            
-            }
-        } );
-        for(Map.Entry<String, Integer> i:sortedList){
-            System.out.println(i.getKey()+" --> "+i.getValue());
-        }
-    }
+public class Reader {
+	public static String raven() {
+		String words = null;
+		try {
+	    	@SuppressWarnings("resource")
+			String word = new Scanner(new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm").openStream(), "UTF-8").useDelimiter("\\A").next();
+	        Map<String,Integer> map = new HashMap<String, Integer>(); 
+	    	String delStr;
+	    	String delStr2;
+	 
+	        delStr = word.substring(0, 1924);
+	        delStr2 = word.substring(10899,30163);
+	        word = word.replace(delStr, "");
+	        word = word.replace(delStr2, "");
+	        word = word.replaceAll("<[^>]*>", "");
+	        word = word.replaceAll("&mdash", "");
+	        word = word.replaceAll("\"", "");
+	        word = word.replaceAll("'", "");
+	        word = word.replaceAll(",", "");
+	        word = word.replaceAll("\\.", "");
+	        word = word.replaceAll("!", "");
+	        word = word.replaceAll(";", " ");
+	        word = word.replaceAll("\\s+", " ").trim();
+	        word = word.toLowerCase();
+	        
+	        
+	        String [] arr = word.split(" ");
+	        	for (int i= 0; i<arr.length; i++) {
+	        		if(map.containsKey(arr[i]) == false) 
+		                map.put(arr[i],1);
+		            else 
+		            {
+		                int count = (int)(map.get(arr[i])); 
+		                map.remove(arr[i]); 
+		                map.put(arr[i],count+1);
+		            }	
+	        } 
+	        	  Set<Map.Entry<String, Integer>> set = map.entrySet();
+	              List<Map.Entry<String, Integer>> sortedList = new ArrayList<Map.Entry<String, Integer>>(set);  
+	              Collections.sort( sortedList, new Comparator<Map.Entry<String, Integer>>() 
+	              {
+	                  public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b ) 
+	                  {
+	                      return (b.getValue()).compareTo( a.getValue() );            
+	                  }
+	              } );
+	              for(Map.Entry<String, Integer> i:sortedList){
+	                  words = words + ("\n"+i.getKey()+" --> "+i.getValue());  
+	        }
+		}
+	catch(Exception FileNotFound){
+		System.out.println("URL not found");	
+	}
+		return words;
+	}
+	public char getFirstChar() {
+		return Reader.raven().charAt(0);
+	}
 }
